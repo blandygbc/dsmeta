7202c1dfe5c0419f97a38644e88a6fe3253fbeeb
Gabriel <gabrielblandy@gmail.com>
2022-09-06 17:38:48 -0300

    First commit

---
+++ backend/src/test/java/com/glelk/dsmeta/DsmetaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.glelk.dsmeta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsmetaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
