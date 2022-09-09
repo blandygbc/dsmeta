70658991fd64cd0a734c79aaf71f5a0dd0c02268
Gabriel <gabrielblandy@gmail.com>
2022-09-09 15:07:50 -0300

    Add twilio sms notifications

--- backend/src/main/java/com/glelk/dsmeta/controllers/SaleController.java
+++ backend/src/main/java/com/glelk/dsmeta/controllers/SaleController.java
@@ -4,12 +4,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.glelk.dsmeta.entities.Sale;
 import com.glelk.dsmeta.services.SaleService;
+import com.glelk.dsmeta.services.SmsService;
 
 @RestController
 @RequestMapping(value = "/sales")
@@ -17,11 +19,24 @@ public class SaleController {
     @Autowired
     private SaleService service;
 
+    @Autowired
+    private SmsService smsService;
+
     @GetMapping
     public Page<Sale> findSales(
             @RequestParam(value = "minDate", defaultValue = "") String minDate,
             @RequestParam(value = "maxDate", defaultValue = "") String maxDate,
             Pageable pageable) {
         return service.findSales(minDate, maxDate, pageable);
+    }
+
+    @GetMapping("/notifications")
+    public void notifyBySms() {
+        smsService.sendSms();
+    }
+
+    @GetMapping("/{saleId}/notifications")
+    public void notifySaleBySms(@PathVariable Long saleId) {
+        smsService.sendSaleBySms(saleId);
     }
 }
---
+++ backend/src/main/java/com/glelk/dsmeta/services/SmsService.java
@@ -0,0 +1,69 @@
+package com.glelk.dsmeta.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import com.glelk.dsmeta.entities.Sale;
+import com.glelk.dsmeta.repositories.SaleRepository;
+import com.twilio.Twilio;
+import com.twilio.rest.api.v2010.account.Message;
+import com.twilio.type.PhoneNumber;
+
+@Service
+public class SmsService {
+
+    @Value("${twilio.sid}")
+    private String twilioSid;
+
+    @Value("${twilio.key}")
+    private String twilioKey;
+
+    @Value("${twilio.phone.from}")
+    private String twilioPhoneFrom;
+
+    @Value("${twilio.phone.to}")
+    private String twilioPhoneTo;
+
+    @Autowired
+    private SaleRepository saleRepository;
+
+    public void sendSms() {
+
+        Twilio.init(twilioSid, twilioKey);
+
+        PhoneNumber to = new PhoneNumber(twilioPhoneTo);
+        PhoneNumber from = new PhoneNumber(twilioPhoneFrom);
+
+        Message message = Message.creator(to, from, "Teste").create();
+
+        System.out.println(message.getSid());
+    }
+
+    public void sendSaleBySms(Long saleId) {
+
+        Sale sale = saleRepository.findById(saleId).orElse(new Sale());
+        StringBuilder stringBuilder = new StringBuilder();
+        if (sale.getSellerName() != null) {
+            stringBuilder.append("O vendedor ")
+                    .append(sale.getSellerName())
+                    .append(" foi destaque em ")
+                    .append(sale.getDate().getMonthValue())
+                    .append("/")
+                    .append(sale.getDate().getYear())
+                    .append(" com um total de R$ ")
+                    .append(sale.getAmount());
+        } else {
+            stringBuilder.append("O vendedor não foi encontrado");
+        }
+
+        Twilio.init(twilioSid, twilioKey);
+
+        PhoneNumber to = new PhoneNumber(twilioPhoneTo);
+        PhoneNumber from = new PhoneNumber(twilioPhoneFrom);
+
+        Message message = Message.creator(to, from, stringBuilder.toString()).create();
+
+        System.out.println(message.getSid());
+    }
+}
