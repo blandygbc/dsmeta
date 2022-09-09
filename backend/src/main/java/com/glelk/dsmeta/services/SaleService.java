0b065d062777fb8e5b7583abbba491d26a7b64d6
Gabriel <gabrielblandy@gmail.com>
2022-09-09 13:15:50 -0300

    Add API

---
+++ backend/src/main/java/com/glelk/dsmeta/services/SaleService.java
@@ -0,0 +1,26 @@
+package com.glelk.dsmeta.services;
+
+import java.time.LocalDate;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Page;
+import org.springframework.stereotype.Service;
+
+import com.glelk.dsmeta.entities.Sale;
+import com.glelk.dsmeta.repositories.SaleRepository;
+
+@Service
+public class SaleService {
+
+    @Autowired
+    private SaleRepository repository;
+
+    public Page<Sale> findSales(String minDate, String maxDate, Pageable pageable) {
+        LocalDate today = LocalDate.now();
+        LocalDate min = minDate.equals("") ? today.minusDays(365) : LocalDate.parse(minDate);
+        LocalDate max = maxDate.equals("") ? today : LocalDate.parse(maxDate);
+
+        return repository.findSales(min, max, pageable);
+    }
+}
